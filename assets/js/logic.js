b7755d3dfb851c9f8007b256462130b859f653cd
Jennifer Marcum <Jen@jennifermarcum.com>
2017-10-14 10:26:08 -0700

    Updated files

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,19 +1,11 @@
-// Shows modal on window load
-/*window.onload = function(){        
-    $('#myModal').modal('show')
-};*/
 
+$("#find-breweries-btn").on("click", function(event) {
+var zipcode = $("#zipcode-input").val().trim();
+var queryURL = "http://api.brewerydb.com/v2/locations?postalCode=" + zipcode + "&key=0e8760e7ad6c26f9098f623ca7d0b5a7";
 
-//var type = $('#location-type-select').val();
-//var locality = $("#city-input").val();
-//var region = $("#state-input").val();
-
-var queryURL = "http://api.brewerydb.com/v2/?location=sandiego&key=0e8760e7ad6c26f9098f623ca7d0b5a7";
-
-
-function breweryInfo(){
-var proxyCall = function(queryURL){
-	return $.ajax({
+        event.preventDefault();
+        var proxyCall = function(queryURL){
+		return $.ajax({
 		method: "POST",
 		dataType: "json",
 		url: "https://proxy-cbc.herokuapp.com/proxy",
@@ -26,13 +18,15 @@ var proxyCall = function(queryURL){
 proxyCall(queryURL).done(function(response) {
 
 	console.log(response);
-	/*var breweryDiv = $("<div class='brewery'>");
-	var breweryName = response.name;
-	var pOne = $("<p>").text("Brewery: " + breweryName);breweryDiv.append(pOne);
-	breweryDiv.append(pOne);*/
+	var breweryDiv = $("<div class='brewery'>");
+	var brewery = response.data.data[0].name;
+	var address = response.data.data[0].streetAddress;
+	var website = response.data.data[0].website;
+	var pOne = $("<p>").text("Brewery: " + brewery);breweryDiv.append(pOne);
+	var pTwo = $("<p>").text("Address: " + address);breweryDiv.append(pTwo);
+	var pThree = $('<p>').text("Website: " + website);breweryDiv.append(pThree);
+	$('#content').prepend(breweryDiv);
 });
-
-}
-breweryInfo();
+});
 
 
