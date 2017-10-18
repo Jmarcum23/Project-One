53643956c368d9424eeab0d2ff8e3b91d8589673
Jennifer Marcum <Jen@jennifermarcum.com>
2017-10-18 09:06:27 -0700

    Updated files

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,57 @@
+//Global Variables
+var latitude;
+var longitude;
+var coords = [];
+var arrayLength;
+var breweryNum = 1;
+var map;
+
+
+//Display Brewery Info
+function displayBreweryInfo() {
+	var zipcode = $("#zipcode-input").val().trim();
+	var queryURL = "http://api.brewerydb.com/v2/locations?postalCode=" + zipcode + "&key=0e8760e7ad6c26f9098f623ca7d0b5a7";
+	var apiKey = "272f3002d2ba42214bb93754e572957f692647d0";
+
+	        var proxyCall = function(queryURL){
+			return $.ajax({
+			method: "GET",
+			dataType: "json",
+			url: csProxyUtils.buildProxyUrl(apiKey, queryURL),
+	  	});	
+	};
+
+	proxyCall(queryURL).done(function(response) {
+
+		console.log(response);
+		console.log(response.data.length);
+		var breweryDiv = $("<div class='brewery'>");
+		arrayLength = response.data.length;
+
+		for (var i = 0; i < arrayLength; i++) {
+			var brewery = response.data[i].name;
+			var address = response.data[i].streetAddress;
+			var locality = response.data[i].locality;
+			var region = response.data[i].region;
+			var postalCode = response.data[i].postalCode;
+			var phone = response.data[i].phone;
+			var website = response.data[i].website;	
+			latitude = response.data[i].latitude;
+			longitude = response.data[i].longitude;	
+			coords[i] = [latitude,longitude];
+			var pOne = $("<p>").html("<h3><span class=\"brewery-num\">" + breweryNum++ + "</span> " + brewery + "</h3>");breweryDiv.append(pOne);
+			var pTwo = $("<p>").html(address + "<br>" + locality + ", " + region + " " + postalCode + "<br>" + phone);breweryDiv.append(pTwo);
+			var pThree = $('<p>').html("<a class=\"btn\" id=\"website-btn\" href=" + website + " target=\"_blank\">website" + "</a><br>" + "<hr>");breweryDiv.append(pThree);
+			$('#form-content').prepend(breweryDiv);
+		}
+		dropPins();
+	});
+}
+
 //Google Map API 
 function initMap() {
 	var uluru = {lat: 32.7157, lng: -117.1611};
-	var map = new google.maps.Map(document.getElementById('map'), {
+	map = new google.maps.Map(document.getElementById('map'), {
 	  zoom: 10,
 	  scrollwheel: false,
 	  center: uluru
@@ -28,49 +78,31 @@ function toggleBounce() {
     }
 }
 
-//Display Brewery Info
-function displayBreweryInfo() {
-	var zipcode = $("#zipcode-input").val().trim();
-	var queryURL = "http://api.brewerydb.com/v2/locations?postalCode=" + zipcode + "&key=0e8760e7ad6c26f9098f623ca7d0b5a7";
-	var apiKey = "272f3002d2ba42214bb93754e572957f692647d0";
-
-	        var proxyCall = function(queryURL){
-			return $.ajax({
-			method: "GET",
-			dataType: "json",
-			url: csProxyUtils.buildProxyUrl(apiKey, queryURL),
-	  	});	
-	};
-
-	proxyCall(queryURL).done(function(response) {
-
-		console.log(response);
-		console.log(response.data.length);
-		var breweryDiv = $("<div class='brewery'>");
-		var arrayLength = response.data.length;
-		var breweryNum = 1;
-		
-		for (var i = 0; i < arrayLength; i++) {
-			var brewery = response.data[i].name;
-			var address = response.data[i].streetAddress;
-			var locality = response.data[i].locality;
-			var region = response.data[i].region;
-			var postalCode = response.data[i].postalCode;
-			var phone = response.data[i].phone;
-			var website = response.data[i].website;	
-			var pOne = $("<p>").html("<h3><span class=\"brewery-num\">" + breweryNum++ + "</span> " + brewery + "</h3>");breweryDiv.append(pOne);
-			var pTwo = $("<p>").html(address + "<br>" + locality + ", " + region + " " + postalCode + "<br>" + phone);breweryDiv.append(pTwo);
-			var pThree = $('<p>').html("<a class=\"btn\" id=\"website-btn\" href=" + website + " target=\"_blank\">website" + "</a><hr>");breweryDiv.append(pThree);
-			$('#form-content').prepend(breweryDiv);
-		}
-	});
+function dropPins () {
+	for (var i = 0; i < arrayLength; i++){
+		var latLng = new google.maps.LatLng(coords[i][0],coords[i][1]);
+        var marker = new google.maps.Marker({
+	        position: latLng,
+	        map: map,
+	        draggable: true,
+			animation: google.maps.Animation.DROP,
+			icon: {
+			url:'file:///users/jmarcum/documents/school/project-one/assets/images/map-icon@2x.png',
+			size: new google.maps.Size(40, 52),
+			scaledSize: new google.maps.Size(40, 52),
+			}
+		});
+		console.log(marker);
+		console.log(coords);
+	}
 }
+
 
 //What happens when find breweries button is clicked
 $("#find-breweries-btn").on("click", function(event) {
 	event.preventDefault();
+	breweryNum = 1;
 	$('#form-content').empty();
 	displayBreweryInfo();
 });
-
 
